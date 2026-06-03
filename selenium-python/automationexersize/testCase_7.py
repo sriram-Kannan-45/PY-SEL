72721bc4a8513285f74521f34ffe3ddad23c69b6
sriram-Kannan-45 <wavene20@gmail.com>
2026-06-03 17:07:32 +0530

    testCase_7

---
+++ selenium-python/automationexersize/testCase_7.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+
+import time
+
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+driver = webdriver.Chrome()
+
+driver.maximize_window()
+
+driver.get("https://automationexercise.com/")
+
+homePage = "https://automationexercise.com/"
+
+if driver.current_url == homePage :
+
+    print ("Verifide home page")
+
+    time.sleep(5)
+
+    testCasePage = driver.find_element(By.XPATH , '//a[text()=" Test Cases"]')
+
+    testCasePage.click()
+
+    time.sleep(5)
+    
+    testCaseText = driver.find_element(By.XPATH,"//b[contains(text(),'Test Cases')]")
+
+    if testCaseText.is_displayed():
+
+        print("testCase page verified")
+
+    else :
+
+        print ("testcase page not verified")
+
+else :
+
+    print ("not Verify home page")
