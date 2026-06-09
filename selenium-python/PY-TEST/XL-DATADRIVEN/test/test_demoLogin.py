263b7b201d19717491b70f62e2c99a52fca6cbc9
sriram-Kannan-45 <wavene20@gmail.com>
2026-06-09 17:05:06 +0530

    log

--- selenium-python/PY-TEST/XL-DATADRIVEN/test/test_demoLogin.py
+++ selenium-python/PY-TEST/XL-DATADRIVEN/test/test_demoLogin.py
@@ -1,77 +1,87 @@
 
 import pytest
 
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from utilities import excelReader
-
-
 import utilities.logCreater as logCreater
 
+
 logger = logCreater.log_generator()
+
 
 @pytest.mark.parametrize(
     "username,password",
     excelReader.get_data(
-    "ExcelFiles/loginData.xlsx",
-    "loginData"
-)
+        "ExcelFiles/loginData.xlsx",
+        "loginData"
+    )
 )
 def test_ValLogin(username, password):
 
-    logger.info("Starting Login Test")
+    logger.info("========== Login Test Started ==========")
 
     driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 10)
-
-    driver.get("https://www.demoblaze.com/index.html")
     driver.maximize_window()
 
-    logger.info("Application launched")
+    wait = WebDriverWait(driver, 20)
 
-    wait.until(
-        EC.element_to_be_clickable((By.ID, "login2"))
-    ).click()
+    try:
 
-    logger.info("Login popup opened")
+        driver.get("https://www.demoblaze.com/index.html")
+        logger.info("Application launched successfully")
 
-    wait.until(
-        EC.visibility_of_element_located((By.ID, "loginusername"))
-    ).send_keys(username)
+        wait.until(
+            EC.element_to_be_clickable((By.ID, "login2"))
+        ).click()
 
-    logger.info(f"Username entered: {username}")
+        logger.info("Login popup opened")
 
-    wait.until(
-        EC.visibility_of_element_located((By.ID, "loginpassword"))
-    ).send_keys(password)
+        wait.until(
+            EC.visibility_of_element_located((By.ID, "loginusername"))
+        ).send_keys(username)
 
-    logger.info("Password entered")
+        logger.info(f"Username entered: {username}")
 
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//button[text()='Log in']"))
-    ).click()
+        wait.until(
+            EC.visibility_of_element_located((By.ID, "loginpassword"))
+        ).send_keys(password)
 
-    logger.info("Login button clicked")
+        logger.info("Password entered")
 
-    logout = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//a[text()='Log out']"))
-    ).text
+        wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[text()='Log in']"))
+        ).click()
 
-    assert logout == "Log out"
+        logger.info("Login button clicked")
 
-    logger.info("Login successful")
+        logout = wait.until(
+            EC.visibility_of_element_located((By.XPATH, "//a[text()='Log out']"))
+        ).text
 
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//a[text()='Log out']"))
-    ).click()
+        assert logout == "Log out", "Login Failed"
 
-    logger.info("Logout successful")
+        logger.info("Login successful")
 
-    driver.quit()
+        wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//a[text()='Log out']"))
+        ).click()
 
-    logger.info("Browser closed")
+        logger.info("Logout successful")
+
+    except Exception as e:
+
+        logger.error(f"Test Failed : {e}")
+
+        raise
+
+    finally:
+
+        driver.quit()
+
+        logger.info("Browser closed")
+        logger.info("========== Login Test Completed ==========")
 
--- selenium-python/PY-TEST/XL-DATADRIVEN/utilities/logCreater.py
+++ selenium-python/PY-TEST/XL-DATADRIVEN/utilities/logCreater.py
@@ -1,13 +1,27 @@
+
 import logging
 
 def log_generator():
 
-    logging.basicConfig(
-        filename=r"D:\PY-SEL\selenium-python\PY-TEST\XL-DATADRIVEN\utilities\testLogReports.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        force=True
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
     )
 
-    return logging.getLogger()
+    file_handler = logging.FileHandler(
+        r"D:\PY-SEL\selenium-python\PY-TEST\XL-DATADRIVEN\utilities\testLogReports.log"
+    )
+
+    console_handler = logging.StreamHandler()
+
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.handlers.clear()
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
