9d6be10e8f103da8c7de1ee8b9f5c1ad7257504c
sriram-Kannan-45 <wavene20@gmail.com>
2026-06-09 22:02:03 +0530

    config solved

--- selenium-python/PY-TEST/tutorialNinja-DataDriven/util/config_read.py
+++ selenium-python/PY-TEST/tutorialNinja-DataDriven/util/config_read.py
@@ -1,9 +1,18 @@
 from configparser import ConfigParser
+import os
 
 def get_config(category, key):
 
     config = ConfigParser()
 
-    config.read(r"ExcelData\config.ini")
+    base_path = os.path.dirname(os.path.dirname(__file__))
+    config_path = os.path.join(base_path, "ExcelData", "config.ini")
+
+    print("Config Path:", config_path)
+    print("File Exists:", os.path.exists(config_path))
+
+    config.read(config_path)
+
+    print("Sections:", config.sections())
 
     return config.get(category, key)
